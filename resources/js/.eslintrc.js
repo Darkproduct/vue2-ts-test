a9aa506950615254b2199c9d5fce69b37d531f20
Johannes Bier <johannes.bier@siemens.com>
2021-10-14 13:50:47 +0200

    Fix multible parserOptions in eslint config

--- resources/js/.eslintrc.js
+++ resources/js/.eslintrc.js
@@ -1,9 +1,13 @@
+// https://www.digitalocean.com/community/tutorials/vuejs-vue-eslint-prettier
+
 module.exports = {
+  root: true,
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    "plugin:vue/essential", // Use this if you are using Vue.js 2.x.
-    "plugin:prettier/recommended" // Last extension
+    // "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:vue/essential",                  // Use this if you are using Vue.js 2.x.
+    "plugin:prettier/recommended",           // Last extension
   ],
   parser: "vue-eslint-parser", // Specifies the ESLint parser
   parserOptions: {
@@ -18,9 +22,7 @@ module.exports = {
       // and vue interpolations (e.g. `{{variable}}`).
       // If not specified, the parser determined by `<script lang ="...">` is used.
       "<template>": "espree"
-    }
-  },
-  parserOptions: {
+    },
     ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
     sourceType: "module" // Allows for the use of imports
   },
